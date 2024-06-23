746be56102ca217c756373cf5cb904e54d99e776
Rupendra Raghu <rupendraraghu@gmail.com>
2024-06-23 15:37:35 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html'
+})
+export class AppComponent {
+    resultsData = signal<{
+        year : number,
+        interest : number,
+        valueEndOfYear : number,
+        annualInvestment : number,
+        totalInterest : number,
+        totalAmountInvested : number
+      }[] | undefined>(undefined);
+    
+}
